75b42258eeb304fa1703dc9d665ec136edfeb70e
Aaron Berdanier <aaron.berdanier@gmail.com>
2017-03-26 10:30:06 -0400

    update documentation in the functions from shaver.R

--- shaver.R
+++ shaver.R
@@ -8,23 +8,26 @@ get_cols = function(x, delim=",") tryCatch(read.table(textConnection(x), sep=del
 find_data_rows = function(f, delim=",", min_rows=10, col_spec=0.9, row_sens=0.5){
   cc = numeric() # the number of columns in each row
   rr = 1 # initial row
+  # rc is the row count, cc is the column count, first is the first row with that class, ctyp is a list of column types
   classes = tibble::tibble(rc=numeric(), cc=numeric(), first=numeric(), ctyp=list())
-  # 50% threshold for consensus on 'data row'
-  # Keep adding while the number of rows is less than tolerance and the file is not done
+  # Finding consensus on 'data row'
+  # Keep adding while the number of data rows is less than the sensitivity (or less than minimum rows) and the file is not done
   while(all(classes$rc/sum(classes$rc)<row_sens) || sum(classes$rc)<min_rows){
     ll = readr::read_lines(f, skip=rr-1, n_max=1) # the given line
     if(length(ll)==0) break # if end of file
     gcc = get_cols(ll, delim)[1,]
     cc = append(cc, length(gcc)) # add to number of cols per row
+    # the classes in the new row
     newclass = as.character(unlist(lapply(gcc, class)))
+    # check for agreement with other row types
     agreement = function(x) length(which(x==newclass))/max(length(x),length(newclass))>col_spec
     # alternative 100% agreement = function(x) all(x==newclass)
     matchclass = sapply(classes$ctyp, agreement) # match row class
-    if(any(matchclass)){
+    if(any(matchclass)){ # increment that row type
       classes$rc[which(matchclass)] = classes$rc[which(matchclass)]+1
-    }else{
+    }else{ # add a new row type
+      # (as long as it is not a row with a single text column and blank commas to fill column space)
       if(length(newclass)>1 && !(newclass[1]=="factor" && all(newclass[-1]=="logical"))){
-        # catch edge case where first column is text and commas are automatically added to fill space
         classes = tibble::add_row(classes, rc=1, cc=length(newclass), first=rr, ctyp=list(newclass))
       }
     }
@@ -39,8 +42,9 @@ shave_csv = function(f, delim=",", min_rows=10, col_spec=0.9, row_sens=0.5, meth
   # method = approach for grabbing the head row,
   # "first": Assume first row with correct dimensions is header row
   # "continuous": Assume row before first data row is header row
+  # Find the data rows
   dr = find_data_rows(f, delim, min_rows, col_spec, row_sens)
-  srow = dr$dd$first-1 # number of row to skip
+  srow = dr$dd$first-1 # number of rows to skip
   ww = which(dr$cc==dr$dd$cc) # which rows in sample match length of data row
   if(method=="first"){
     # also, check if header row classes match data row classes
@@ -51,7 +55,9 @@ shave_csv = function(f, delim=",", min_rows=10, col_spec=0.9, row_sens=0.5, meth
   }else{
     stop('Please specify a method in c("first","continuous")')
   }
+  # column names from the header row
   cnames = as.character(unlist(get_cols(readr::read_lines(f, n_max=1, skip=hrow-1))))
+  # read in the data file
   x = readr::read_csv(f, skip=srow, col_names=cnames, col_types=readr::cols())
   return(x)
 }
